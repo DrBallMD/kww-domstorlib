f309bb0f3706dd140d8bf4307e4712880eb84982
pavel <pavel@localhost>
2013-06-27 17:36:08 +0700

    *

--- autoload.php
+++ autoload.php
@@ -2,10 +2,11 @@
 $path = dirname(__FILE__);
 
 require_once($path.'/lib/sp/loader.php');
-require_once($path.'/domstorlib.php');
 
 $loader = new SP_Loader();
 $loader->registerPrefix('SP', $path.'/lib');
 $loader->registerPrefix('Domstor', $path.'/lib');
 $loader->registerPrefix('Doctrine', $path.'/lib');
 $loader->register();
+
+require_once($path.'/domstorlib.php');
