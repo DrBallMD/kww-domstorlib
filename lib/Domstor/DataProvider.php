93c4aaaa545abce00cd19642150f1355cac39d13
pavel <pavel@localhost>
2013-07-05 16:17:51 +0700

    [*] creating dataprovider

---
+++ lib/Domstor/DataProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Description of DataProvider
+ *
+ * @author pahhan
+ */
+class Domstor_DataProvider
+{
+    /**
+     *
+     * @var Doctrine_Cache_Driver
+     */
+    protected $cacheDriver;
+
+    protected $cache_time;
+
+    function __construct(Doctrine_Cache_Driver $cacheDriver, $cache_time) {
+        $this->cacheDriver = $cacheDriver;
+        $this->cache_time = $cache_time;
+    }
+
+    public function getData($url)
+    {
+        if( $this->cacheDriver ) {
+            $id = $this->generateCacheId($url);
+            if( $this->cacheDriver->contains($id) ) {
+                $content = $this->cacheDriver->fetch($id);
+            }
+            else {
+                $content = $this->readUrl($url);
+                $this->cacheDriver->save($id, $content);
+            }
+        }
+        else {
+
+        }
+    }
+
+    protected function generateCacheId($url, array $params = array())
+    {
+        return md5($url);
+    }
+
+    protected function readUrl($url)
+    {
+        return file_get_contents($url);
+    }
+
+    protected function contentToData($content)
+    {
+        $data = base64_decode($content);
+        return unserialize($data);
+    }
+}
+
