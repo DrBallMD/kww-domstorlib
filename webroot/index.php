842b9761c4bc62e7de901e78916e0c77095859cf
pavel <pavel@localhost>
2013-07-19 13:11:15 +0700

    [*] modify index

--- webroot/index.php
+++ webroot/index.php
@@ -1,50 +1,108 @@
 <?php
+// к сожалению не все E_NOTICE исправлены :(
+error_reporting(E_ALL & !E_NOTICE);
+
+// подключаем автозагрузчик
 require_once(__DIR__.'/../domstorlib/autoload.php');
 
+// создаем объект Domstor
 $domstor = new Domstor();
-$domstor->setMyId(1);
+
+// устанавливаем идентификатор организации
+$domstor->setMyId(0);
+
+// устанавливаем идентификатор местоположения по умолчанию
 $domstor->setHomeLocation(2004);
 
-$driver = extension_loaded('apc')? new Doctrine_Cache_Apc() : new Doctrine_Cache_Array();
-$domstor->setCacheDriver($driver);
-$domstor->setCacheTime(600);
+// конфигурируем кэш-драйверы - рекомендуется включить для увеличения быстродействия
+//$driver = extension_loaded('apc')? new Doctrine_Cache_Apc() : new Doctrine_Cache_Array();
+//$domstor->setCacheDriver($driver);
+//$domstor->setCacheTime(600);
 
+// получаем параметры object, action необходимые для отображения
+/**
+ * Возможные значения object:
+ * flat - квартиры
+ * house - дома и коттеджи
+ * land - земля и дачи
+ * garage - гаражи и парковки
+ * commerce - вся коммерческая
+ * Коммерческую можно разделить на
+ *  trade - торговую
+ *  office - офисную
+ *  product - производственную
+ *  storehouse - складскую
+ *  landcom - земля
+ *  complex - комплекс
+ *  other - прочие
+ *
+ * Возможные значения для action:
+ * sale - продают
+ * rent - сдают
+ * purchase - купят
+ * rentuse - снимут
+ * exchange - обмен (возможен только для flat, house)
+ * new - новостройки (только для flat)
+ */
 $object = isset($_GET['object'])? $_GET['object'] : null;
 $action = isset($_GET['action'])? $_GET['action'] : null;
+
+// получаем текущую страницу
 $page = isset($_GET['page'])? $_GET['page'] : 1;
+
+// получаем идентификатор объекта
 $id = isset($_GET['id'])? $_GET['id'] : null;
 
+// эти переменный выводим во view
 $html = '';
 $title = '';
-if( $object and $action )
+$filter = '';
+$count = '';
+
+// проверяем $object $action
+if( Domstor_Helper::checkEstateAction($object, $action) )
 {
+    // если определен id - покажем страницу с детальной информацией
     if( $id )
     {
-        //error_reporting(E_ALL & !E_NOTICE);
-        $detail = $domstor->getObject($object, $action, $id);
+        // запрашиваем объект детального описания
+        $detail = $domstor->getDetail($object, $action, $id);
+
+        // проверяем что получен
         if( $detail )
         {
-            $detail->showSecondHead(true);
+            // получаем html-код детального описания
             $html = $detail->render();
+            // получает тайтл
             $title = $detail->getPageTitle();
         }
-        $filter = '';
+        else
+        {
+            // показать 404
+        }
+
     }
-    else
+    else // иначе список
     {
-        //if( isset($_GET['ids']) ) $domstor->addParam('id', $_GET['ids'] );
-        $params = isset($_GET['ids'])? array('id' => $_GET['ids']) : array();
-        $list = $domstor->getList($object, $action, $page, $params);
-        $list->deleteField('contact_phone');
-        $list->deleteField('code');
+        // получем объект список
+        $list = $domstor->getList($object, $action, $page);
+        // получаем html-код списка
         $html = $list->getHtml();
+        // получаем объект фильтр
         $filter = $list->getFilter();
+        // получаем общее количество найденых записей
+        $count = $domstor->getCount($object, $action);
     }
 }
-
-echo $title, '<br/>';
-echo $filter;
-echo $html;
-echo $domstor->getCount($object, $action);
+else
+{
+    // обработать ошибку
+}
 ?>
-<pre><?php $list->dumpFields() ?></pre>
+
+<!-- где-то во view... -->
+<?php echo $title ?><br/>
+<?php echo $filter ?> <!-- у объекта filter есть волшебный метод __toString -->
+<?php if($count): ?>Всего: <?php echo $count ?><?php endif ?>
+<?php echo $html ?>
+
