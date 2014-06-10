1a1468e1432b7dca0abc3ad83ee2b875a4c0e118
pavel <pavel@localhost>
2014-06-10 18:09:35 +0700

    fix empty district becomes location name

--- lib/Domstor/Transformer/Supply/CityDistrict.php
+++ lib/Domstor/Transformer/Supply/CityDistrict.php
@@ -9,7 +9,9 @@ class Domstor_Transformer_Supply_CityDistrict implements Domstor_Transformer_Int
 {
     public function get($data)
     {
-        return empty($data['district'])? ( empty($data['location_name'])? '' : $data['location_name']) : $data['district'];
+        return empty($data['district']) ?
+            (empty($data['location_name']) || $data['location_name'] == $data['master_city'] ? '' : $data['location_name']) :
+            $data['district'];
     }
 }
 
